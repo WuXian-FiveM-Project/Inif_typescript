3b942c71c0ff547e23732963f532d8834756a38d
RULoN <rluo2018@gmail.com>
2022-06-05 09:37:28 +0800

    init repo

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'bodacious'
+game 'gta5'
+
+name 'FiveM Typescript Boilerplate'
+description 'A boilerplate for using Typescript in FiveM'
+author 'Remco Troost (d0p3t)'
+url 'https://github.com/d0p3t/fivem-ts-boilerplate'
+
+client_script 'dist/client/*.client.js'
+server_script 'dist/server/*.server.js'
